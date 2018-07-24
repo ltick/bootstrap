fd83fd328494a06184e2f272861253d323791895
Sam <sam42@outlook.com>
2018-07-25 00:04:14 +0800

    update

---
+++ app/dummy-server/main.go
@@ -0,0 +1,12 @@
+// Copyright © 2017 Ding Jing <dingjing@lianjia.com>
+// this file is part of {{ .appName }}
+
+package main
+
+import (
+	"github.com/ltick/dummy/app/dummy-server/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
